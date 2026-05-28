37c868ede680ac7e9d7ec1087be6e6f962c1ed2e
octaviog44 <octaviog2308@gmail.com>
2026-05-28 20:00:56 -0300

    Creacion de repository

---
+++ src/main/java/com/utn/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.utn.repository;
+
+import com.utn.entities.Categoria;
+
+public class CategoriaRepository
+        extends BaseRepository<Categoria> {
+
+    public CategoriaRepository() {
+        super(Categoria.class);
+    }
+}
